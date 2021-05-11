8015de1c5aa91b4b5d2535ed9b87305f76f5728c
mason <kuangminshen@gmail.com>
2021-05-11 22:42:35 +1000

    components init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+
 import './App.css';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
+
 function App() {
+  const [input, setInput] = useState('');
+
+  const onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  const onSubmit = () => {
+    console.log('click');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className="particles" params={particlesOptions} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm onInputChange={onInputChange} onSubmit={onSubmit} />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
