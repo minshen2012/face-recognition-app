aa187dd457c5860e771d9fa7fe0fb577fd19a57a
mason <kuangminshen@gmail.com>
2021-05-17 14:25:19 +1000

    face box display

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
@@ -37,12 +36,17 @@ function App() {
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
+    //console.log(width, height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
       rightCol: width - clarifaiFace.right_col * width,
       bottomRow: height - clarifaiFace.bottom_row * height,
     };
+  };
+
+  const displayFaceBox = (box) => {
+    setBox(box);
   };
 
   const onInputChange = (event) => {
@@ -53,7 +57,7 @@ function App() {
     setImageUrl(input);
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, input)
-      .then((response) => calculateFaceLocation(response))
+      .then((response) => displayFaceBox(calculateFaceLocation(response)))
       .catch((err) => console.log(err));
   };
 
@@ -67,7 +71,7 @@ function App() {
         onInputChange={onInputChange}
         onButtonSubmit={onButtonSubmit}
       />
-      <FaceRecognition imageUrl={imageUrl} />
+      <FaceRecognition box={box} imageUrl={imageUrl} />
     </div>
   );
 }
